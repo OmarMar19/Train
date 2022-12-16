373b3a68b9cc31371929017a747217a56367d1b8
MohammedAlolabi <102738915+Ghost0o7@users.noreply.github.com>
2022-12-16 16:55:24 +0300

    Update Train.h

--- Train/Train.h
+++ Train/Train.h
@@ -1,14 +1,94 @@
 #pragma once
 #include "point.h"
 
-class Train
+class Train1
 {
 public:
 	static	void DrawTrain(Point leftPoint, Point leftUpPoint ,Point rightUpPoint,Point rightPoint);
 	static  void DrawWalls(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
 	static  void DrawDoor(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint); 
-
+	static	void Window(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
 };
+class Train2
+{
+public:
+	static	void DrawTrain(Point leftPoint, Point leftUpPoint ,Point rightUpPoint,Point rightPoint);
+	static  void DrawWalls(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+	static  void DrawDoor(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint); 
+	static	void Window(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+};
+
+class Train3
+{
+public:
+	static	void DrawTrain(Point leftPoint, Point leftUpPoint ,Point rightUpPoint,Point rightPoint);
+	static  void DrawWalls(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+	static  void DrawDoor(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint); 
+	static	void Window(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+};
+
+class Train4
+{
+public:
+	static	void DrawTrain(Point leftPoint, Point leftUpPoint ,Point rightUpPoint,Point rightPoint);
+	static  void DrawWalls(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+	static  void DrawDoor(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint); 
+	static	void Window(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+};
+
+class Train5
+{
+public:
+	static	void DrawTrain(Point leftPoint, Point leftUpPoint ,Point rightUpPoint,Point rightPoint);
+	static  void DrawWalls(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+	static  void DrawDoor(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint); 
+	static	void Window(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+};
+
+class Train6
+{
+public:
+	static	void DrawTrain(Point leftPoint, Point leftUpPoint ,Point rightUpPoint,Point rightPoint);
+	static  void DrawWalls(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+	static  void DrawDoor(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint); 
+	static	void Window(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+};
+
+class Train7
+{
+public:
+	static	void DrawTrain(Point leftPoint, Point leftUpPoint ,Point rightUpPoint,Point rightPoint);
+	static  void DrawWalls(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+	static  void DrawDoor(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint); 
+	static	void Window(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+};
+
+class Train8
+{
+public:
+	static	void DrawTrain(Point leftPoint, Point leftUpPoint ,Point rightUpPoint,Point rightPoint);
+	static  void DrawWalls(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+	static  void DrawDoor(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint); 
+	static	void Window(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+};
+
+class Train9
+{
+public:
+	static	void DrawTrain(Point leftPoint, Point leftUpPoint ,Point rightUpPoint,Point rightPoint);
+	static  void DrawWalls(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+	static  void DrawDoor(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint); 
+	static	void Window(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+};
+class Train10
+{
+public:
+	static	void DrawTrain(Point leftPoint, Point leftUpPoint ,Point rightUpPoint,Point rightPoint);
+	static  void DrawWalls(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+	static  void DrawDoor(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint); 
+	static	void Window(Point leftPoint, Point upperLeftPoint,Point upperRightPoint, Point rightPoint);
+};
+
 
 
 
